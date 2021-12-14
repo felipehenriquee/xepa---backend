4dec535383e9625f94c5d4bcd25612b749064b71
felipehenriquee <fh27.95@gmail.com>
2021-12-13 22:41:19 -0300

    troca de bd

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,22 @@
+// module.exports = {
+//     dialect: 'mysql',
+//     host: 'localhost',
+//     port: '3306',
+//     username: 'root',
+//     password: '@@mysql123',
+//     database: 'xepa',
+//     define: {
+//         timestamps: true,
+//     }
+// }
+
 module.exports = {
     dialect: 'mysql',
-    host: 'localhost',
-    port: '3306',
-    username: 'root',
-    password: '@@mysql123',
-    database: 'xepa',
+    host: 'mysql.concertodenataldacatedral.com.br',
+    
+    username: 'concertodenata',
+    password: 'xepa123',
+    database: 'concertodenata',
     define: {
         timestamps: true,
     }
