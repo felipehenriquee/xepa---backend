ea85fc321a513a87f077421ca6951bb33a7bafcf
felipehenriquee <fh27.95@gmail.com>
2022-01-12 08:50:09 -0300

    ajuste calculo desconto

--- src/services/ProdutoService.js
+++ src/services/ProdutoService.js
@@ -155,7 +155,7 @@ module.exports = {
         
     },
     CalculaDesconto(original, promocional){
-        return Math.round((promocional*100)/original)
+        return Math.round((original-promocional*100)/original)
     }
     
     
