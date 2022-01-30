ca6f827ad73976e1509c2ead7d1045861cb8dd4a
felipehenriquee <fh27.95@gmail.com>
2022-01-30 10:51:53 -0300

    bd heroku

--- src/config/database.js
+++ src/config/database.js
@@ -23,12 +23,13 @@
 // }
 module.exports = {
     dialect: 'mysql',
-    host: 'sql717.main-hosting.eu',
+    host: 'us-cdbr-east-05.cleardb.net',
     
-    username: 'u984285269_xepauser',
-    password: 'Xep@1234',
-    database: 'u984285269_xepa',
+    username: 'b265cee843a1f8',
+    password: '87d0a991',
+    database: 'heroku_a5389e04c1e40a0',
     define: {
         timestamps: true,
     }
 }
+// mysql://b265cee843a1f8:87d0a991@us-cdbr-east-05.cleardb.net/heroku_a5389e04c1e40a0?reconnect=true
