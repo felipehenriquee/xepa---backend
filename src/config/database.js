2855c41b9d98608e68ab92689d9927c53ddb1e6c
felipehenriquee <79702834+felipehenriquee@users.noreply.github.com>
2023-02-16 15:23:03 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,35 +1,12 @@
-// module.exports = {
-//     dialect: 'mysql',
-//     host: 'localhost',
-//     port: '3306',
-//     username: 'root',
-//     password: '@@mysql123',
-//     database: 'xepa',
-//     define: {
-//         timestamps: true,
-//     }
-// }
-
-// module.exports = {
-//     dialect: 'mysql',
-//     host: 'mysql.concertodenataldacatedral.com.br',
-    
-//     username: 'concertodenata',
-//     password: 'xepa123',
-//     database: 'concertodenata',
-//     define: {
-//         timestamps: true,
-//     }
-// }
 module.exports = {
     dialect: 'mysql',
-    host: 'us-cdbr-east-05.cleardb.net',
+    host: '',
     
-    username: 'b265cee843a1f8',
-    password: '87d0a991',
-    database: 'heroku_a5389e04c1e40a0',
+    username: '',
+    password: '',
+    database: '',
     define: {
         timestamps: true,
     }
 }
-// mysql://b265cee843a1f8:87d0a991@us-cdbr-east-05.cleardb.net/heroku_a5389e04c1e40a0?reconnect=true
+
